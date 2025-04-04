d9c900345c7f151775ade4ff8a4a90adabde9e7e
sonikaur21 <skaur1902@gmail.com>
2025-04-04 19:50:55 -0400

    updated js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 function updateTime() {
   let madridElement = document.querySelector("#madrid");
-  let madridElementDate = madridElement.querySelector(".date");
-  let madridElementTime = madridElement.querySelector(".time");
-  let madridTime = moment().tz("Europe/Madrid");
-  madridElementDate.innerHTML = madridTime.format("MMMM Do YYYY");
-  madridElementTime.innerHTML = `${madridTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
-
+  if (madridElement) {
+    let madridElementDate = madridElement.querySelector(".date");
+    let madridElementTime = madridElement.querySelector(".time");
+    let madridTime = moment().tz("Europe/Madrid");
+    madridElementDate.innerHTML = madridTime.format("MMMM Do YYYY");
+    madridElementTime.innerHTML = `${madridTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoElementDate = tokyoElement.querySelector(".date");
-  let tokyoElementTime = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
-  tokyoElementDate.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoElementTime.innerHTML = `${tokyoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
-
+  if (tokyoElement) {
+    let tokyoElementDate = tokyoElement.querySelector(".date");
+    let tokyoElementTime = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoElementDate.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoElementTime.innerHTML = `${tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
   let atlantaElement = document.querySelector("#atlanta");
-  let atlantaElementDate = atlantaElement.querySelector(".date");
-  let atlantaElementTime = atlantaElement.querySelector(".time");
-  let atlantaTime = moment().tz("America/New_York");
-  atlantaElementDate.innerHTML = atlantaTime.format("MMMM Do YYYY");
-  atlantaElementTime.innerHTML = `${atlantaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+  if (atlantaElement) {
+    let atlantaElementDate = atlantaElement.querySelector(".date");
+    let atlantaElementTime = atlantaElement.querySelector(".time");
+    let atlantaTime = moment().tz("America/New_York");
+    atlantaElementDate.innerHTML = atlantaTime.format("MMMM Do YYYY");
+    atlantaElementTime.innerHTML = `${atlantaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
-
 updateTime();
 setInterval(updateTime, 100);
+
+function displaySelect(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+        </div>`;
+}
+let cityselectElement = document.querySelector("#cityselect");
+
+cityselectElement.addEventListener("change", displaySelect);
